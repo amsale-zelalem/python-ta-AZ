65bd34b04b8c945c9f6db2b8dbefa40016dfc833
Amsale Zelalem Bayih <amsale-zelalem@users.noreply.github.com>
2022-06-20 18:34:08 +0200

    app.py

--- src/app.py
+++ src/app.py
@@ -4,7 +4,18 @@ import os
 
 def prime(s):
     # your code goes here
-
+    flag=False
+    if s>1:
+        for i in range(2, s):
+            if (s % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
+            # break out of loop
+            break
+    if flag == True:
+        print("number is not prime")
+    else:
+        print("number is prime")
 def solution(s):
     return prime(s)
 
